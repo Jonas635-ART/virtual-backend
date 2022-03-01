98cb10ba001164df691bcb8428db203ec5ba9ace
Jonas635-ART <jonasazula8901mail.com>
2022-03-01 11:26:53 -0500

    tarea

--- día-1/05-otros-operadores.py
+++ día-1/05-otros-operadores.py
@@ -26,9 +26,39 @@ print((10>5) or (10< 20))
 verduras = ['api', 'lechuga', 'zapallo']
 verduras2 = verduras
 verduras3 = ['api', 'lechuga', 'zapallo']
+
+verduras[0] = 'perejil'
+verduras[1] = 'mango'
+# el metodo copy() hace una copia del contenido de la variable pero se 
+# guarda en una nueva posicion de la memoria
+verduras4 = verduras.copy()
+verduras4[0] = 'huacatay'
 print(verduras2 is verduras)
+print(verduras)
+print(verduras2)
 print(verduras3 is verduras)
 
+
+print('la posicion de la variable verduras:',id(verduras))
+print('la posicion de la variable verduras:',id(verduras2))
+print('la posicion de la variable verduras:',id(verduras4))
+
+# Al hablas de variables basicas al hacer la copia comparten el mismo espacio en la 
+# memoria al modificarla python le asigna un nuevo espacio
+nombre='jose'
+nombre2 = 'carlos'
+print(nombre2 is nombre)
+print(id(nombre2))
+print(id(nombre))
+nombre2 = 1
+print(nombre)
+print(id(nombre2))
+print(id(nombre))
+
+#validar si el nombre es eduardo y colombiano o japones
+nombre='eduardo'
+pais='japones'
+print(nombre == 'eduardo' and (pais == 'japones' or pais == 'colombia'))
 
 
 
