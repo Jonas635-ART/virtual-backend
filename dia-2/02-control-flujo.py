42b5ac9be3a4ffc79585d150066381c4a8dae56d
Jonas635-ART <jonasazula8901mail.com>
2022-03-01 22:52:06 -0500

    Agregar2

---
+++ dia-2/02-control-flujo.py
@@ -0,0 +1,42 @@
+# If - Else
+edad = int(input('ingrese su edad'))
+
+if (edad > 18):  
+
+    print('es mayor de edad')
+    print('otra impresion')
+
+elif edad > 15:
+    print('Ingrese a escuela')
+
+elif edad > 19:
+    print('Ingrese a vacunacion')
+
+else:
+    print('eres menor de edad')
+
+print('finalizado')
+
+
+ingreso =int(input('ingrese su sueldo'))
+
+if (ingreso > 500):
+    print('No recibe nada, gana demasiado')
+
+elif ingreso >= 250 <= 500: 
+    print('recibe el bono')
+
+else: 
+    print('recibe bono mas balon de gas')   
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dia-2/03-bucle-for.py
@@ -0,0 +1,24 @@
+notas = [10, 20, 30]
+
+
+#for
+for nota in notas:
+    print(nota)
+
+for numero in range(10):
+    print(numero)
+
+for numero in range(5, 10):
+    print(numero)
+    
+for numero in range(5,10,2):
+    print(numero)
+
+
+
+
+
+
+
+
+
