9637fdfa5b7818e59e5b829ab745e4bccc49c0d4
Jonas635-ART <jonasazula8901mail.com>
2022-03-07 22:10:18 -0500

    agregar7

--- dia1/app.py
+++ dia1/app.py
@@ -1,15 +1,26 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def estado():
+    hora_del_servidor = datetime.now()
+    return{
+            'status': True,
+            'hour': hora_del_servidor.strftime('%d/%m/%Y %H:%M:%S') 
+    }     
+
+@app.route('/clientes')
+def obtener_clientes():
+    return{
+        'message': 'Exito'
+    }
+
+
+
 app.run(debug=True)
-
-
-
-
-
-
-
 
 
 
