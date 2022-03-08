b01a242703fff7d95c74c5f88c714bb229a58e35
Jonas635-ART <jonasazula8901mail.com>
2022-03-08 09:42:48 -0500

    agrgar archivos semana02

--- dia1/app.py
+++ dia1/app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from datetime import datetime
 
 app = Flask(__name__)
 
-
+clientes = []
 @app.route('/')
 def estado():
     hora_del_servidor = datetime.now()
@@ -12,10 +12,16 @@ def estado():
             'hour': hora_del_servidor.strftime('%d/%m/%Y %H:%M:%S') 
     }     
 
-@app.route('/clientes')
+@app.route("/clientes", methods=['POST'])
 def obtener_clientes():
-    return{
-        'message': 'Exito'
+    print(request.method)
+    print(request.get_json())
+    data = request.get_json()
+    clientes.append(data)
+    
+    return {
+        'message': 'Exito',
+        'cliente': data
     }
 
 
