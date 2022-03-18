992a6e2e7dcf54fbb4247196c875d2dc9da66113
Jonas635-ART <jonasazula8901mail.com>
2022-03-17 19:09:14 -0500

    agregar3

---
+++ dtos/dto_prueba.py
@@ -0,0 +1,42 @@
+from config import validador
+from marshmallow import fields, validate
+
+class ValidadorPrueba(validador.Schema):
+    nombre = fields.Str(validate=validate.Length(max=10))
+    apellido = fields.Str()
+    edad = fields.Int()
+    soltero = fields.Bool()
+    
+    
+    
+   # class Meta:
+   #     fields = ['nombre','apellido']
+
+class ValidarUsuarioPrueba(validador.Schema):
+    nombre = fields.Str()
+    apellido= fields.Str()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dtos/ingrediente_dto.py
@@ -0,0 +1,45 @@
+from config import validador
+from models.ingredientes import Ingrediente
+from marshmallow_sqlalchemy import auto_field
+from marshmallow import validate
+
+class IngredienteRequestDTO(validador.SQLAlchemyAutoSchema):
+    nombre = auto_field(validate= validate.And(validate.Length(min=1), validate.Regexp("[A-Z]|[a-z]+")))
+  
+  
+    class Meta:
+      model = Ingrediente
+
+
+class IngredienteResponseDTO(validador.SQLAlchemyAutoSchema):
+
+    class Meta:
+      model = Ingrediente
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
