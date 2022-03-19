c9f4822bb7a9ab5e34c9507e5cc7fdad91ec3b2f
Jonas635-ART <jonasazula8901mail.com>
2022-03-18 20:27:13 -0500

    agregar5

--- controllers/recetas.py
+++ controllers/recetas.py
@@ -1,25 +1,53 @@
 from flask_restful import Resource, request
 from models.recetas import Receta
-from dtos.receta_dto import RecetaRequestDTO
-
+from dtos.receta_dto import RecetaRequestDTO, RecetaResponseDTO
+from config import conexion
 class RecetasController(Resource):
     def post(self):
         body = request.get_json()
         try:
             data = RecetaRequestDTO().load(body)
-
+            nuevaReceta = Receta(
+                nombre = data.get('nombre'),
+                estado = data.get('estado'),
+                comensales = data.get('comensales'),
+                duracion = data.get('duracion'),
+                dificultad = data.get('dificultad')
+            )
+         
+            conexion.session.add(nuevaReceta)
+            conexion.session.commit()
+            
+            respuesta = RecetaResponseDTO().dump(nuevaReceta)
             return {
-                'message':'Receta exitosamente creada'
+                'message':'Receta exitosamente creada',
+                'content': respuesta
             }, 201
 
         except Exception as e:
+            conexion.session.rollback()
             return {
                 'message':'Error al crear la receta',
                 'content': e.args
             }
 
-
-
+    def get(self):
+      # TODO: agrega paginacion
+      recetas = conexion.session.query(Receta).all()
+      respuesta = RecetaResponseDTO(many=True).dump(recetas)
+      return {
+        'message':'las recetas son:',
+        'content': respuesta
+    }
+class BuscarRecetaController(Resource):
+    def get(self):
+        query_params = request.args
+        print(query_params.get('nombre'))
+        recetas = conexion.session.query(Receta).filter_by(**query_params).all()
+        print(recetas)
+        return {
+            'message': ''
+        }
 
 
 
--- dtos/receta_dto.py
+++ dtos/receta_dto.py
@@ -1,13 +1,22 @@
 from config import validador
 from models.recetas import Receta
+from marshmallow import fields, validate
 
 
 class RecetaRequestDTO(validador.SQLAlchemyAutoSchema):
     class Meta:
         model = Receta
-        
+         
+class RecetaResponseDTO(validador.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Receta
 
-
+class BuscarRecetaRequestDTO(validador.Schema):
+    nombre = fields.String(required=False)
+    estado = fields.Boolean(required=False)
+    comensales = fields.Integer(required=False)
+    dificultad = fields.String(required=False, validate=validate.ContainsOnly(choices=['FACIL', 'INTERMEDIO', 
+    'DIFICIL', 'EXTREMO']))
 
 
 
