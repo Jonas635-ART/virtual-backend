525c76adabb5eb4e6326b632d33527853d27b8cc
Jonas635-ART <jonasazula8901mail.com>
2022-05-07 12:47:25 -0500

    Agergarcontenedores

---
+++ Dockerfile
@@ -0,0 +1,52 @@
+FROM node:18-alpine
+
+WORKDIR /app
+
+COPY . /app/
+
+RUN npm install
+
+CMD ["npm", "run", "start:dev"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT ?? 3000
 console.log(process.env.MONGO_URL);
 
 mongoose
-    .connect(process.env.MONGO_URL)
+    .connect(process.env.MONGO_URL, { dbName: process.env.DBNAME })
     .then((valor) => {
       console.log("Conectado a la base de datos 😎");
     })
